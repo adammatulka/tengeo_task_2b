405831573851730e62333519e18bbe51b2269398
Adam Matulka <matulka.adam@gmail.com>
2021-01-19 13:24:30 +0100

    hookbin POST request added

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,28 @@ import { Heading } from './heading';
 export const Main = () => {
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
+  var xhr = new XMLHttpRequest();
 
-  const onSubmit = (e) => {
-    alert('A name was submitted: ' + email + ' ' + phone);
+  xhr.open("POST", "https://hookb.in/mZRdGYVQ0DSeqq710eD3", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+
+ /*  const onSubmit = (e) => {
+    alert('Email was submitted: ' + email + ' phone was submitted' + phone);
     e.preventDefault();
+  }
+*/
+   const onSubmit = (e) => {
+    e.preventDefault();
+    alert('Both your e-mail: ' + email + ' AND phone number: ' + phone +' just got logged in console and POSTed on Hookbin rn my dude <3');
+
+      console.log('mail in console: '+ email);  
+      console.log('phone in console: '+ phone);
+
+    var data = JSON.stringify({email,phone});
+  
+    xhr.send(data);
+    
   }
 
   const onClear = () => {
